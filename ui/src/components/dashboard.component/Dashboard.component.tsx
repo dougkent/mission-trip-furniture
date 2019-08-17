f68f05aaac8359dc38d1f9b733c1a81afcbfe7ad
Doug Kent <doug.kent88@gmail.com>
2019-08-17 18:40:08 -0500

    Finish boostrapping app.

---
+++ ui/src/components/dashboard.component/Dashboard.component.tsx
@@ -0,0 +1,59 @@
+// React
+import React from 'react';
+
+// AWS
+import Amplify from 'aws-amplify';
+import aws_exports from '../../aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+
+// Configure
+Amplify.configure(aws_exports);
+
+const signUpConfig = {
+    hideAllDefaults: true,
+    signUpFields: [
+        {
+            label: 'Email',
+            key: 'email',
+            required: true,
+            displayOrder: 1,
+            type: 'string',
+        },
+
+        {
+            label: 'Name',
+            key: 'name',
+            required: true,
+            displayOrder: 2,
+            type: 'string',
+        },
+        {
+            label: 'Username',
+            key: 'username',
+            required: true,
+            displayOrder: 3,
+            type: 'string',
+        },
+        {
+            label: 'Password',
+            key: 'password',
+            required: true,
+            displayOrder: 4,
+            type: 'password',
+        }
+    ]
+}
+
+class DashboardComponent extends React.Component {
+    render() {
+        return (
+            <h1>
+                Dashboard
+            </h1>
+        );
+    }
+}
+
+export default withAuthenticator(DashboardComponent, {
+    signUpConfig: signUpConfig
+});
--- ui/src/containers/App.tsx
+++ ui/src/containers/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import HomeComponent from '../components/home/Home.component';
-import DashboardComponent from '../components/dashboard/Dashboard.component';
+import HomeComponent from '../components/home.component/Home.component';
+import DashboardComponent from '../components/dashboard.component/Dashboard.component';
+import NavComponent from '../components/nav.component/Nav.component';
 import './App.scss';
 
 class App extends React.Component {
@@ -10,14 +11,7 @@ class App extends React.Component {
     return (
       <Router>
         <div className="container">
-          <header>
-            <Link to="/" className="nav-item">
-              Mission Trip Furniture
-            </Link>
-            <Link to="/dashboard" className="nav-item">
-              Dashboard
-            </Link>
-          </header>
+          <NavComponent />
           <Route exact path="/" component={HomeComponent} />
           <Route path="/dashboard" component={DashboardComponent} />
         </div>
