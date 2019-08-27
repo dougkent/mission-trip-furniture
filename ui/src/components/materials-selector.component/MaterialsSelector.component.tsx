34c90eee8ece21b5835702eb33ef15abcd00609c
dougk@genebygene.com <dougk@genebygene.com>
2019-08-27 09:42:41 -0500

    Working commit.

--- ui/src/components/materials-selector.component/MaterialsSelector.component.tsx
+++ ui/src/components/materials-selector.component/MaterialsSelector.component.tsx
@@ -1,13 +1,35 @@
 // React
 import React from 'react';
 
+//AWS
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import Connect from 'aws-amplify-react';
+import aws_exports from './aws-exports';
+
 // Material
 import Chip from '@material-ui/core/Chip';
 
+// React Select
+import Select from 'react-select';
+
 //MTF
 import { SelectedMaterial } from '../../models/selected-material';
+import { listMaterials } from '../../graphql/queries';
+import { ListMaterialsQuery } from '../../services/API';
+
+//Configure
+Amplify.configure(aws_exports);
+
+export interface Material {
+    id: string,
+    name: string,
+}
 
 class MaterialsSelectorComponent extends React.Component {
+    state: { selectedMaterials: Material[], materials: Material[] } = {
+        selectedMaterials: [],
+        materials: []
+    }
 
     private sampleMaterialData: SelectedMaterial[] = [
         {
@@ -20,23 +42,58 @@ class MaterialsSelectorComponent extends React.Component {
         }
     ];
 
-    handleDelete = (selectedMaterial: SelectedMaterial): void => {
-        // TODO Handle Delete
+    handleChange = (selectedMaterial: Material) => {
+        const selectedMaterials = this.state.selectedMaterials;
+        selectedMaterials.push(selectedMaterial);
+
+        this.setState({ selectedMaterials });
+
+        console.log(`Option selected:`, selectedMaterial, 'Options selected: ', selectedMaterials);
+    };
+
+    handleDelete = (selectedMaterial: Material) => {
+        let selectedMaterials = this.state.selectedMaterials;
+        selectedMaterials = selectedMaterials.filter((material) => material.id != selectedMaterial.id);
+
+        this.setState({ selectedMaterials });
+
         console.log(selectedMaterial);
+    }
+
+    onInputChange = (newValue: string) => {
+        // TODO: Handle Input change
+        console.log('InputChange', newValue);
     }
 
     render() {
         return (
             <div>
                 <h3>Materials Needed</h3>
-                {this.sampleMaterialData.map(selectedMaterial => {
+                <Connect query={graphqlOperation(listMaterials)}>
+                    {
+                        ({ data, loading }: { data: ListMaterialsQuery, loading: boolean }) => {
+                            if (data.listMaterials) {
+                                this.setState({ materials: data.listMaterials.items });
+                            }
+
+                            <Select closeMenuOnSelect={false}
+                                isMulti
+                                name="selectedMaterials"
+                                isLoading={loading}
+                                options={this.state.materials}
+                            />
+                        }
+                    }
+
+                </Connect>
+                {/* {this.sampleMaterialData.map(selectedMaterial => {
                     return (
                         <Chip key={selectedMaterial.id}
                             label={selectedMaterial.name}
                             onDelete={() => this.handleDelete(selectedMaterial)}
                         />
                     )
-                })}
+                })} */}
             </div>
         )
     }
