9c39ce3a5b432b7a7a3ae534a345947029b1fbac
Doug Kent <doug.kent88@gmail.com>
2020-03-14 20:06:35 -0500

    feat: Favoriting backend updating favoriteCount Trigger

--- amplify/backend/function/mftFavoriteTrigger/src/index.js
+++ amplify/backend/function/mftFavoriteTrigger/src/index.js
@@ -1,52 +1,64 @@
 const AWS = require('aws-sdk');
 const region = process.env.REGION;
 
-AWS.config.update({ region: region });
-
-const incrementExpression = 'set favoritedCount = favoritedCount + :f';
-const decrementExpression = 'set favoriteCount = favoritedCount - :f';
+const incrementExpression =
+    'set #favoritedCount = #favoritedCount + :favoritedCount';
+const decrementExpression =
+    'set #favoritedCount = #favoritedCount - :favoritedCount';
 
 exports.handler = async (event, context) => {
     //eslint-disable-line
     console.log(JSON.stringify(event, null, 2));
 
-    var docClient = new AWS.DynamoDB.DocumentClient({
+    AWS.config.update({ region: region });
+
+    var ddb = new AWS.DynamoDB({
         apiVersion: '2012-08-10',
+        region: region,
     });
 
-    event.Records.forEach(record => {
-        const favoriteRecord = AWS.DynamoDB.Converter.unmarshall(
-            record.dynamodb.NewImage
-        );
+    for (const index in event.Records) {
+        const record = event.Records[index];
+
+        let favoriteRecord;
+
+        if (record.eventName === 'INSERT') {
+            favoriteRecord = AWS.DynamoDB.Converter.unmarshall(
+                record.dynamodb.NewImage
+            );
+        } else {
+            favoriteRecord = AWS.DynamoDB.Converter.unmarshall(
+                record.dynamodb.OldImage
+            );
+        }
 
         const params = {
             TableName: `${process.env.API_MISSIONTRIPFURNITURE_PLANTABLE_NAME}-${process.env.ENV}`,
             Key: {
-                id: favoriteRecord.favoritePlanId,
+                id: {
+                    S: favoriteRecord.favoritePlanId,
+                },
             },
             UpdateExpression:
                 record.eventName === 'INSERT'
                     ? incrementExpression
                     : decrementExpression,
+            ExpressionAttributeNames: {
+                '#favoritedCount': 'favoritedCount',
+            },
             ExpressionAttributeValues: {
-                ':f': 1,
+                ':favoritedCount': {
+                    N: '1',
+                },
             },
         };
 
-        docClient.update(params, (err, data) => {
-            if (err) {
-                console.error(
-                    'Unable to update plan favorited count. Error JSON: ',
-                    JSON.stringify(err, null, 2)
-                );
-            } else {
-                console.log(
-                    'Update plan favorited count succeeded: ',
-                    JSON.stringify(data, null, 2)
-                );
-            }
-        });
-    });
+        try {
+            await ddb.updateItem(params).promise();
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
     context.done(null, 'Successfully processed DynamoDB record'); // SUCCESS with message
 };
